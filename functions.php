5d1cb6002a9174d7fcb9ade4248c536935971d3c
Muhammad Andrean Saputra <andreannproject@gmail.com>
2023-05-14 10:42:25 +0700

    add registration.php

--- functions.php
+++ functions.php
@@ -79,3 +79,35 @@ function search($keyword)
                 ";
     return query($query);
 }
+
+function registration($data)
+{
+    global $conn;
+
+    $username = strtolower(stripslashes($data["username"]));
+    $password = mysqli_real_escape_string($conn, $data["password"]);
+    $confirm_password = mysqli_real_escape_string($conn, $data["confirm_password"]);
+
+    $result = mysqli_query($conn, "SELECT username FROM admin WHERE username = '$username'");
+
+    if (mysqli_fetch_assoc($result)) {
+        echo "<script>
+                alert('username telah terdaftar')
+                </script>";
+        return false;
+    }
+
+    if ($password !== $confirm_password) {
+        echo "<script>
+                alert('konfirmasi password tidak sesuai')
+                </script>";
+        return false;
+    }
+
+    // Encryption Password
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    mysqli_query($conn, "INSERT INTO admin VALUES ('','$username','$password')");
+
+    return mysqli_affected_rows($conn);
+}
